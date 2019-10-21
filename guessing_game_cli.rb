58f7ab82f6198c8947913919f29c0bd0ce73ca00
Kyle Cole <kylepcole@gmail.com>
2019-10-21 20:01:19 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   solution = rand(1..6)
   
-  puts "Guess a number between 1 and 6:"
+  puts "Guess a number between 1 and 6"
   answer = gets.chomp
   
   elsif answer == solution.to_s 
