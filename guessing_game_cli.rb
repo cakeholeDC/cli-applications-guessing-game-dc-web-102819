2d9f436ae2b71c111a746fa8bc1aec221fd7df39
Kyle Cole <kylepcole@gmail.com>
2019-10-21 19:28:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,10 +1,14 @@
 def run_guessing_game
   puts "run_guessing_game"
   
-  answer = rand(1..10)
-  puts answer
+  solution = rand(1..10)
+  puts solution
   
   puts "Guess a number between 1 and 6"
-  answer
+  answer = gets.chomp
+  
+  if answer == solution 
+    puts "You guessed the correct number!"
+  else if answer == "exit"
   
 end
